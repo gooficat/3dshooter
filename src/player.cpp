255b779234c2d842bf09a1ffc86169cf82bbffb6
Hasan Sabri <hsabri2007@gmail.com>
2025-06-30 23:53:31 +0100

    improve movement code almost fixed

--- src/player.cpp
+++ src/player.cpp
@@ -5,9 +5,8 @@ float PLAYER_SPEED = 7.0f;
 
 Player::Player(glm::vec3 position, glm::vec3 rotation) :
 				position(position), rotation(rotation) {
-	head = position + glm::vec3(0.0f, 2.0f, 0.0f);
-	collider = new SphereCollider(head, 1.0f);
-	camera = new PerspectiveCamera(75.0f, 800.0f/600.0f, 0.1f, 100.0f, head);
+	collider = new SphereCollider(glm::vec3(0.0f, 1.5f, 0.0f), 1.0f);
+	camera = new PerspectiveCamera(75.0f, 800.0f/600.0f, 0.1f, 1000.0f, head);
 	accel = PLAYER_ACCEL_DEFAULT;
 }
 
@@ -18,25 +17,24 @@ Player::~Player() {
 void Player::update(GLFWwindow* window, Shader& shader, float deltaTime, glm::vec2 mouseRelative) {
 	camera->yaw += mouseRelative.x * deltaTime * 50.0f;
 	camera->pitch -= mouseRelative.y * deltaTime * 50.0f;
-	camera->pitch = glm::clamp(camera->pitch, -89.0f, 89.0f);
 	camera->updateCameraVectors();
-	direction = {0.0f, 0.0f, 0.0f};
+	direction = glm::vec3(0.0f);
 	if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		direction.z -= 1.0f;
+		direction += camera->front;
 	if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		direction.z += 1.0f;
+		direction -= camera->front;
 	if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		direction.x -= 1.0f;
+		direction -= glm::normalize(glm::cross(camera->front, camera->up));
 	if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		direction.x += 1.0f;
+		direction += glm::normalize(glm::cross(camera->front, camera->up));
 	
-	direction = glm::rotateY(direction, glm::radians(camera->yaw));
-	velocity = glm::mix(velocity, direction * PLAYER_SPEED, accel * deltaTime);
+	direction.y = 0;
+	//direction = glm::rotateY(direction, glm::radians(camera->yaw));
+	velocity = direction;
 	
 	position += velocity;
 	
-	head = position + glm::vec3(0.0f, 5.0f, 0.0f);
-	camera->position = head;
-	collider->position = head;
+	camera->position =  position + glm::vec3(0.0f, 1.5f, 0.0f);
+	collider->position =  position + glm::vec3(0.0f, 1.0f, 0.0f);
 	camera->use(shader);
 }
